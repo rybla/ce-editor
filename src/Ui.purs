16285b006011378c9ca826deb8f82dbd365f2a3d
Henry Blanchette <henry@monkeyface.com>
2025-04-16 11:44:01 -0400

    removed old stuff (like Component and AppV0)

--- src/Ui.purs
+++ src/Ui.purs
@@ -6,11 +6,9 @@ import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Exception (throw)
 import Ui.App as App
-import Ui.Example.List as Example.List
 
 main :: Effect Unit
 main = case "App" of
   "App" -> App.main
-  "Example.List" -> Example.List.main
   label -> liftEffect $ throw $ "unrecognized main label: " <> show label
 
